db5f614ee807d1018f73bb7c47ca02be521190dd
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-01-31 09:56:55 -0600

    phase 0 pac 3 intro to functions lab

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string){
+    // todo
+    return string.toUpperCase(); }
+
+function whisper(string) {
+    //todo
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    // todo
+    console.log(string.toUpperCase())
+;}
+
+function logWhisper(string){
+    // todo
+    console.log(string.toLowerCase())
+;}
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can\'t hear you!";
+    }
+    if (string === string.toUpperCase()) {
+        return "YES INDEED!";
+    }
+    if (string === "Let\'s have dinner together!") {
+        return "I would love to!";
+    }
+
+
+
+}
